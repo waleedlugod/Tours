f27a749c477aceeeea11181b5e3b41b8fd801e2e
Waleed Lugod <lugodwaleed@gmail.com>
2022-10-15 17:42:16 +0800

    refactored and added refresh button

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,43 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
-import Tours from "./Cards";
 import Header from "./Header";
+import Card from "./Card";
+
+const url = "https://course-api.com/react-tours-project";
 
 function App() {
+	const [tours, setTours] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+	async function getData() {
+		setIsLoading(true);
+		const resp = await fetch(url);
+		const data = await resp.json();
+		setTours(data);
+		setIsLoading(false);
+	}
+
+	function handleRemoveTour(id) {
+		setTours(() => tours.filter((tour) => tour.id != id));
+	}
+
+	useEffect(() => {
+		getData();
+	}, []);
+
 	return (
 		<div className="container">
-			<Header />
-			<Tours />
+			<Header
+				empty={tours.length <= 0}
+				getData={getData}
+				isLoading={isLoading}
+			/>
+			<>
+				{tours.map((tour) => {
+					return (
+						<Card key={tour.id} {...tour} handleRemoveTour={handleRemoveTour} />
+					);
+				})}
+			</>
 		</div>
 	);
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,29 @@
 import React from "react";
 
-function Header() {
-	return (
-		<div className="header">
-			<h2>Our Tours</h2>
-			<div className="line"></div>
-		</div>
-	);
+function Header({ empty, getData, isLoading }) {
+	if (isLoading) {
+		return (
+			<div className="header">
+				<h2>Loading...</h2>
+			</div>
+		);
+	} else if (empty) {
+		return (
+			<div className="header">
+				<h2>No Tours Left</h2>
+				<button className="refresh-btn" onClick={getData}>
+					Refresh
+				</button>
+			</div>
+		);
+	} else {
+		return (
+			<div className="header">
+				<h2>Our Tours</h2>
+				<div className="line"></div>
+			</div>
+		);
+	}
 }
 
 export default Header;
